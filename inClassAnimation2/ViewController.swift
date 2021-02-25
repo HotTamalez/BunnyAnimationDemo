880da9683138ce9e9531883d15de445246377af4
Hot Tamale <zweakland@setonhill.edu>
2021-02-25 13:10:41 -0500

    Initial Commit

---
+++ inClassAnimation2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inClassAnimation2
+//
+//  Created by zweakland on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
