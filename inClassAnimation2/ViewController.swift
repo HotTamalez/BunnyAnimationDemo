19f914816ea53e5bdc8cc8819af2bc986c73e815
Hot Tamale <zweakland@setonhill.edu>
2021-03-04 00:30:36 -0500

    Final version

--- inClassAnimation2/ViewController.swift
+++ inClassAnimation2/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bunnyView: UIImageView!
+    
+    @IBOutlet weak var speedSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let bunnyImages: [UIImage] = [
+            UIImage(named: "frame-1")!,
+            UIImage(named: "frame-2")!,
+            UIImage(named: "frame-3")!,
+            UIImage(named: "frame-4")!,
+            UIImage(named: "frame-5")!,
+            UIImage(named: "frame-6")!,
+            UIImage(named: "frame-7")!,
+            UIImage(named: "frame-8")!,
+            UIImage(named: "frame-9")!,
+            UIImage(named: "frame-10")!,
+            UIImage(named: "frame-11")!,
+            UIImage(named: "frame-12")!,
+            UIImage(named: "frame-13")!,
+            UIImage(named: "frame-14")!,
+            UIImage(named: "frame-15")!,
+            UIImage(named: "frame-16")!,
+            UIImage(named: "frame-17")!,
+            UIImage(named: "frame-18")!,
+            UIImage(named: "frame-19")!,
+            UIImage(named: "frame-20")!
+        ]
+        
+        bunnyView.animationImages = bunnyImages
+        bunnyView.animationDuration = 1.0
+        
     }
-
-
+    
+    @IBAction func setSpeed(_ sender: Any) {
+        bunnyView.animationDuration = TimeInterval(3.5 - speedSlider.value)
+        bunnyView.startAnimating()
+    }
+    
+    @IBAction func bunnyHopButton(_ sender: Any) {
+        if (bunnyView.isAnimating) {
+            bunnyView.stopAnimating()
+        } else {
+            bunnyView.startAnimating()
+        }
+    }
+    
 }
 
